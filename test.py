db18b0f47f068badf4ab01dbc39b8b9a5037aa03
Budhadev Das <82575957+Mikun007@users.noreply.github.com>
2026-04-06 22:02:00 +0530

    fast api

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+from fastapi import FastAPI
+import json
+app = FastAPI()
+
+#lets create helper function to read ptient records from a file
+def load_data():
+      with open("patient.json", "r") as f:
+            data = json.load(f)
+      return data
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello World"}
+
+@app.get("/mikun_das")
+def mikun_das():
+    return {"message": "This is a FastAPI application created by Mikun Das"}
+
+@app.get("/patient_record")
+def patient_record():
+      return {"message": "This is a FastAPI application to manage patient records."}
 
 
-data = pd.read_csv(r"C:\Users\mikun\Downloads\Data.csv")
+@app.get("/view")
+def view():
+      data = load_data()
+      return data
